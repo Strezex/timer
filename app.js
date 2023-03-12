32aea064a31f1de9e45c437129cb631ed51ee6b0
Strezex <124489820+Strezex@users.noreply.github.com>
2023-03-12 09:07:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-let timer = 0
+let timer = 0;
 let counter = document.getElementById('counter');
 let timerId = '';
 
 function start() {
-    timerId = setInterval(() => {
-        timer++;
-        counter.innerText = timer;
-    }, 1000)
+	// При нажатии на кнопку start запускается таймер
+	timerId = setInterval(() => {
+		// Прибавить к таймеру 1
+		timer++;
+		// Записать в HTML значение переменной timer
+		counter.innerText = timer;
+	}, 100)
 }
+
 function reset() {
-    clearInterval(timerId);
-    timer = 0;
-    counter.innerText = timer;
+	clearInterval(timerId);
+	timer = 0;
+	counter.innerText = timer;
+}
+
+function pause() {
+	clearInterval(timerId);
 }
